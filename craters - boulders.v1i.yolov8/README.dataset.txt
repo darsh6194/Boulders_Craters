56b0dd7aecb8d184b4c3350169b9fdb6c9d38412
darsh6194 <darshanmaharaja1@gmail.com>
2024-12-04 11:14:52 +0530

    Upload

---
+++ craters - boulders.v1i.yolov8/README.dataset.txt
@@ -0,0 +1,6 @@
+# craters & boulders > 2024-07-15 10:04am
+https://universe.roboflow.com/isro-w900k/craters-boulders
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ craters - boulders.v1i.yolov8/model.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import torch
+
+print(torch.cuda.is_available())
+device = torch.device("cuda" if torch.cuda.is_available() else "mps")
+# Load a pre-trained YOLOv8 model (YOLOv8n is the nano version; use YOLOv8s, YOLOv8m for larger models)
+model = YOLO("yolov8n.yaml")  # Initialize a new YOLOv8 model
+
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+# Train the model
+model.train(
+    data=r"D:\Projects\Boulders And Craters\craters - boulders.v1i.yolov8\data.yaml",  # Path to data.yaml
+    epochs=50,                # Number of training epochs
+    imgsz=640,                # Image size (default: 640x640)
+    batch=16,                 # Batch size
+    workers=4,
+    device=device                # Number of dataloader workers
+    )
+
+# Validate the model on the validation set
+metrics = model.val()  # Evaluate performance on validation data
+print("Validation Results:", metrics)
+
+# Test the model on the test set
+results = model.test(data=r"D:\Projects\Boulders And Craters\craters - boulders.v1i.yolov8\data.yaml")  # Evaluate performance on the test dataset
+print("Test Results:", results)
+
+# Predict on new images or datasets
+predict_results = model.predict(
+    source="test/images",  # Path to test images
+    save=True,                    # Save predictions to disk
+    conf=0.25                     # Confidence threshold
+)
+print("Predictions Complete!")
+
+# Export the trained model for deployment
+model.export(format="onnx")  # You can export to other formats like TorchScript, CoreML, etc.
